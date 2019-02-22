6e3b5959a33c857eff57232e2e028eddb95ddbc9
BrandCam <ufc.campbell@gmail.com>
2019-02-22 16:22:58 -0600

    Update work.js
    yea figured that would have been to ez

--- src/templates/work.js
+++ src/templates/work.js
@@ -5,10 +5,7 @@ import Img from 'gatsby-image'
 import { graphql } from 'gatsby'
 import Layout from "../components/layout"
 
-const slideStyle = {
-  height: '250px',
-  width: '400px'
-};
+
 
 
 
@@ -20,7 +17,7 @@ export default ({ data }) => (
         <h1 className="sheet__title">{data.datoCmsWork.title}</h1>
         <p className="sheet__lead">{data.datoCmsWork.excerpt}</p>
         <div className="sheet__slider">
-          <Slider style={slideStyle} infinite={true} slidesToShow={2} arrows>
+          <Slider infinite={true} slidesToShow={2} arrows>
             {data.datoCmsWork.gallery.map(({ fluid }) => (
               <img alt={data.datoCmsWork.title} key={fluid.src} src={fluid.src} />
             ))}
